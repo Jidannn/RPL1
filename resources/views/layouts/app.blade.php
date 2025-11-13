3a8031933cbea6eaa35d97cbed94da91e5b9dfb9
Jidannn <zidanseptian27@gmail.com>
2025-11-13 16:34:06 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://unpkg.com/alpinejs" defer></script>
 </head>
 <body class="bg-gray-50 dark:bg-gray-300">
@@ -15,7 +13,7 @@
     <x-navbar ></x-navbar>
 
     {{-- Main Content --}}
-    <main class="min-h-screen">
+    <main class="min-h-screen pt-36">
         @yield('content')
     </main>
 
